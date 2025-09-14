bf92b4053d8a795339e5656d0b75bfdf4e0a2c06
Noel Ramadan <dreamxscape.management@gmail.com>
2025-09-14 12:03:48 -0600

    Route fixes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
-  basePath: process.env.NODE_ENV === 'production' ? '/umrah-company-marketing-solutions' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/open-source-library' : '',
   images: {
     unoptimized: true, // Disable Next.js image optimization
   },
